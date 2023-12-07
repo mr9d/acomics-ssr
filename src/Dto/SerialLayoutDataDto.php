485579876d9f00858f7b9bf9f633a027f0d4214a
Aleksandr Kozlov <kaa_12@mail.ru>
2023-12-07 22:17:22 +0100

    Add SerialLayoutDataDto basic fields

--- src/Dto/SerialLayoutDataDto.php
+++ src/Dto/SerialLayoutDataDto.php
@@ -3,5 +3,18 @@ namespace Acomics\Ssr\Dto;
 
 class SerialLayoutDataDto
 {
+	//
+	// Идентификаторы комикса
+	//
+	public int $id;
+	public string $name;
+	public string $code;
 
+	//
+	// Базовые элементы оформления
+	//
+	public ?string $headerUrl;
+	public ?string $backgroundUrl;
+	public int $backgroundPosition;
+	public bool $isHeaderManageMenuVisible;
 }
