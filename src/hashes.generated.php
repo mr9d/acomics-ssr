d4355524f6f946ef661c198f74ab40cdc619845e
Aleksandr Kozlov <kaa_12@mail.ru>
2023-12-11 22:01:57 +0100

    Fix serial menu

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -7,6 +7,6 @@ $hashes = array(
   'static/bundle/common.css' => 'eea96df0d13b6a6cd56334d1052be80a',
   'static/bundle/common.js' => '086869a437301b5840620f0958aa6ce4',
   'static/bundle/main.css' => '6169b667f690c7e327c696b3139bbcc3',
-  'static/bundle/serial.css' => 'a7797ab39020303362331b5bf343d8c3',
+  'static/bundle/serial.css' => '7b7cba0e23f8bed45bce14c38f5aabaf',
   'static/bundle/serial.js' => '42f948e0b2d98ae57b05381a20ed22fe',
 );
