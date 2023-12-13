b3457ab95d4bfb5a88c8e3e0ba3c9bd6dea020c8
Aleksandr Kozlov <kaa_12@mail.ru>
2023-12-13 23:31:43 +0100

    Fix z-index for yandex ads

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -4,9 +4,9 @@
 
 global $hashes;
 $hashes = array(
-  'static/bundle/common.css' => 'eea96df0d13b6a6cd56334d1052be80a',
+  'static/bundle/common.css' => '02ac66d48a7f9cbe220b57bf4567461b',
   'static/bundle/common.js' => '43b927be55bc35bce6c74942759d3426',
   'static/bundle/main.css' => '6169b667f690c7e327c696b3139bbcc3',
-  'static/bundle/serial.css' => '2c18c89ac57b401f0aad7f6fedcc234a',
+  'static/bundle/serial.css' => 'c319120c404305ad5af5e721370e03ef',
   'static/bundle/serial.js' => '42f948e0b2d98ae57b05381a20ed22fe',
 );
