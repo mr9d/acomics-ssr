93648018f9d96cd76cdd5314ccfe2597b8354a49
Aleksandr Kozlov <kaa_12@mail.ru>
2023-12-16 16:08:56 +0100

    Fix NR serials about page display

--- src/Dto/SerialAgeRatingDto.php
+++ src/Dto/SerialAgeRatingDto.php
@@ -7,9 +7,9 @@ class SerialAgeRatingDto
 	public int $id;
 	public string $name;
 	public string $nameShort;
-	public string $iconUrl;
+	public ?string $iconUrl;
 
-	public function __construct(int $id, string $name, string $nameShort, string $iconUrl)
+	public function __construct(int $id, string $name, string $nameShort, ?string $iconUrl)
 	{
 		$this->id = $id;
 		$this->name = $name;
