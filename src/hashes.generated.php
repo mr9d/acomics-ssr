3eb1944375d2bb2e76553519ea35d4a12b795f58
Aleksandr Kozlov <kaa_12@mail.ru>
2024-01-01 21:33:29 +0100

    Fix comment expand/collapse on FF

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -7,6 +7,6 @@ $hashes = array(
   'static/bundle/common.css' => '6a697af778bf72188b77dad8d72aa794',
   'static/bundle/common.js' => '43b927be55bc35bce6c74942759d3426',
   'static/bundle/main.css' => '6169b667f690c7e327c696b3139bbcc3',
-  'static/bundle/serial.css' => 'c79e3454146a1096cd7745bcdc7988eb',
+  'static/bundle/serial.css' => '38fac80771f37548c2c6a395b2bcb2a2',
   'static/bundle/serial.js' => 'c043ce025c79e9249c0c5e78f43c8d79',
 );
