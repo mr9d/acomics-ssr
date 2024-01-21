1ca06841d2797b06c6860e5891b33a073617df66
Aleksandr Kozlov <kaa_12@mail.ru>
2024-01-21 12:11:08 +0100

    Fix content layout

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -7,6 +7,6 @@ $hashes = array(
   'static/bundle/common.css' => '12573eca96ac45e2167b14531ac52610',
   'static/bundle/common.js' => '68bb7811c171574dcd878b946c69b0db',
   'static/bundle/main.css' => '6169b667f690c7e327c696b3139bbcc3',
-  'static/bundle/serial.css' => '7c36645d5fbf0c844e76bda57295a746',
+  'static/bundle/serial.css' => '689ad102bcf96d851a1ae168bfac74a1',
   'static/bundle/serial.js' => '7ea1d370ab7839629043eab2605561ae',
 );
