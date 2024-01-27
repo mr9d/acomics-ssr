bee552fb691f1277dd58cc59e74728462bc75307
Aleksandr Kozlov <kaa_12@mail.ru>
2024-01-27 10:06:30 +0100

    Add background delay animation

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -4,7 +4,7 @@
 
 global $hashes;
 $hashes = array(
-  'static/bundle/common.css' => '12573eca96ac45e2167b14531ac52610',
+  'static/bundle/common.css' => '9a63746bb71150338d905e188b9427c7',
   'static/bundle/common.js' => '68bb7811c171574dcd878b946c69b0db',
   'static/bundle/main.css' => '6169b667f690c7e327c696b3139bbcc3',
   'static/bundle/serial.css' => '4ec0c8c2c8042ef47967d6d6ac03de3e',
