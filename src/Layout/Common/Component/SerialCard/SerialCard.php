e9af00ff55940cb426e305ebaad01b84ca1e0da2
Aleksandr Kozlov <kaa_12@mail.ru>
2024-04-15 23:07:04 +0200

    Add SerialCard icons draft code

--- src/Layout/Common/Component/SerialCard/SerialCard.php
+++ src/Layout/Common/Component/SerialCard/SerialCard.php
@@ -4,6 +4,8 @@ namespace Acomics\Ssr\Layout\Common\Component\SerialCard;
 
 use Acomics\Ssr\Dto\CatalogSerialDto;
 use Acomics\Ssr\Layout\AbstractComponent;
+use Acomics\Ssr\Layout\Common\Component\LazyImage\LazyImage;
+use Acomics\Ssr\Util\CatalogStatus;
 use Acomics\Ssr\Util\UrlUtil;
 
 
@@ -20,8 +22,70 @@ class SerialCard extends AbstractComponent
     {
         echo '<article class="serial-card">';
 
-        echo UrlUtil::makeSerialUrl($this->serial->code);
+        $this->renderBanner();
+
+        $this->renderTitle();
 
         echo '</article>'; // serial-card
+    }
+
+    private function renderBanner(): void
+    {
+        echo '<a href="' . UrlUtil::makeSerialUrl($this->serial->code) . '" class="cover" title="Читать комикс ' . $this->serial->name . ' онлайн">';
+
+        (new LazyImage(
+            src: $this->serial->bannerUrl ? $this->serial->bannerUrl : '/design/main/pic/catalog-stub.png?18-07-2014',
+            stubSrc: '/static/img/tail-spin.svg',
+            width: 160,
+            height: 90,
+            alt: 'Обложка комикса ' . $this->serial->name
+        ))->render();
+
+        echo '</a>';
+    }
+
+    private function renderTitle(): void
+    {
+        echo '<h2 class="title">';
+
+        echo '<a href="' . UrlUtil::makeSerialUrl($this->serial->code) . '" title="Читать комикс ' . $this->serial->name . ' онлайн">';
+        echo $this->serial->name;
+        echo '</a>';
+
+        $this->featuredIcon();
+        $this->translateIcon();
+        $this->topVoteIcon();
+
+        echo '</h2>';
+    }
+
+    private function featuredIcon(): void
+    {
+        if ($this->serial->catalogStatus !== CatalogStatus::FEATURED)
+        {
+            return;
+        }
+
+        echo ''; //todo
+    }
+
+    private function translateIcon(): void
+    {
+        if (!$this->serial->isTranslate)
+        {
+            return;
+        }
+
+        echo ''; //todo
+    }
+
+    private function topVoteIcon(): void
+    {
+        if (!$this->serial->isTopEnabled)
+        {
+            return;
+        }
+
+        echo ''; //todo
     }
 }
--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -8,6 +8,6 @@ $hashes = array(
   'static/bundle/common.js' => '68bb7811c171574dcd878b946c69b0db',
   'static/bundle/main.css' => '702e4134acab5f117b794880b179f9e2',
   'static/bundle/main.js' => 'f60dbcf5e30488edd030ac97b26dca9a',
-  'static/bundle/serial.css' => 'a822d584c4588ce88f4bfc8377219d2f',
+  'static/bundle/serial.css' => '86bb0d6250d5162e9d6e480f48705942',
   'static/bundle/serial.js' => '57ee99928c8e542a639ec58253eceb96',
 );
