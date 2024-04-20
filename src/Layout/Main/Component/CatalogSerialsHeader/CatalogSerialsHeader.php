30609e3a5b11199bbd4d03a785dbbcc5b9577997
Aleksandr Kozlov <kaa_12@mail.ru>
2024-04-20 23:24:14 +0200

    Finalize CatalogSerialsHeader

--- src/Layout/Main/Component/CatalogSerialsHeader/CatalogSerialsHeader.php
+++ src/Layout/Main/Component/CatalogSerialsHeader/CatalogSerialsHeader.php
@@ -18,8 +18,18 @@ class CatalogSerialsHeader extends AbstractComponent
 
     public function render(): void
     {
-        echo '<p>CatalogSerialsHeader</p>';
+        echo '<p class="catalog-serials-header">';
+        $this->renderTitle('Описание комикса', 'description', $this->filters->searchSort === 'serial_name');
+        $this->renderTitle('Активность', 'activity', $this->filters->searchSort === 'last_update' || $this->filters->searchSort === 'issue_count');
+        $this->renderTitle('Подписчики', 'subscribe', $this->filters->searchSort === 'subscr_count');
+        echo '</p>'; // catalog-serials-header
     }
 
+    private function renderTitle(string $title, string $cssClass, bool $isSorting): void
+    {
+        echo '<span class="' . ($isSorting ? 'sorting ' : '') . $cssClass . '">';
+        echo $title;
+        echo '</span>';
+    }
 }
 
--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -4,9 +4,9 @@
 
 global $hashes;
 $hashes = array(
-  'static/bundle/common.css' => '3b776956d9b769d9448ab1e300dfed9b',
+  'static/bundle/common.css' => '6bc6c95f982bec9e982a181017f3aa35',
   'static/bundle/common.js' => '78687bb4d822985cdb7520df64fe72c5',
-  'static/bundle/main.css' => '702e4134acab5f117b794880b179f9e2',
+  'static/bundle/main.css' => 'eef115ed701d8fd17d0102a1ffe95694',
   'static/bundle/main.js' => 'f60dbcf5e30488edd030ac97b26dca9a',
   'static/bundle/serial.css' => '56ba756346320249ee530ebdb85c7b14',
   'static/bundle/serial.js' => '57ee99928c8e542a639ec58253eceb96',
