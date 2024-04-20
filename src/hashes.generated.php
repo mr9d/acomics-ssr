0b81192ff703bab28705781d698a155f1026c952
Aleksandr Kozlov <kaa_12@mail.ru>
2024-04-20 20:42:37 +0200

    Finalize adaptive SerialCard

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -4,7 +4,7 @@
 
 global $hashes;
 $hashes = array(
-  'static/bundle/common.css' => '9871f67cb44238d94b98449e81955d23',
+  'static/bundle/common.css' => '3b776956d9b769d9448ab1e300dfed9b',
   'static/bundle/common.js' => '78687bb4d822985cdb7520df64fe72c5',
   'static/bundle/main.css' => '702e4134acab5f117b794880b179f9e2',
   'static/bundle/main.js' => 'f60dbcf5e30488edd030ac97b26dca9a',
