3b3cab8ae0b7525bde08537e28d77d5f9f209e27
Aleksandr Kozlov <kaa_12@mail.ru>
2024-04-30 22:23:51 +0200

    Add SpotlightSerialDto class

--- src/Layout/Main/Component/SpotlightSerialCard/SpotlightSerialCard.php
+++ src/Layout/Main/Component/SpotlightSerialCard/SpotlightSerialCard.php
@@ -2,10 +2,18 @@
 
 namespace Acomics\Ssr\Layout\Main\Component\SpotlightSerialCard;
 
+use Acomics\Ssr\Dto\SpotlightSerialDto;
 use Acomics\Ssr\Layout\AbstractComponent;
 
 class SpotlightSerialCard extends AbstractComponent
 {
+    private SpotlightSerialDto $serial;
+
+    public function __construct(SpotlightSerialDto $serial)
+    {
+        $this->serial = $serial;
+    }
+
     public function render(): void
     {
         echo '<article class="spotlight-serial-card">';
