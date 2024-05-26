90b82aa480dcc6591a9c6850c6678de2850f36c7
Aleksandr Kozlov <kaa_12@mail.ru>
2024-05-26 11:34:15 +0200

    Fix catalog and comment CSS

--- src/hashes.generated.php
+++ src/hashes.generated.php
@@ -4,10 +4,10 @@
 
 global $hashes;
 $hashes = array(
-  'static/bundle/common.css' => '339b045ac3f655006f6e64e0033e9877',
+  'static/bundle/common.css' => '77e84b008c68940a9fa0540acefb3c82',
   'static/bundle/common.js' => 'b63ed83a602107cec0a6c66972f1cada',
   'static/bundle/main.css' => '3cca89bc9a321fb00f7b6e1abc214912',
   'static/bundle/main.js' => '54251a972cef4f783bcbcb976c159460',
-  'static/bundle/serial.css' => '8c00057a26cd0fcb57ebe3dd53bc176e',
+  'static/bundle/serial.css' => '2066c2fb531ec17d3274d64112cda2c7',
   'static/bundle/serial.js' => '8821bcc08e18fe9f0b58a356db515220',
 );
